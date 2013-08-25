d68c7f1e175f81cfe506bf1af29073b53a328fb5
Mike Chen <chagel@gmail.com>
2013-08-25 17:54:29 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beagle::Application.config.secret_token = '41fdffe19f61848a29429a8e86ed5896b227d1cc0afdad4715ef85104d7a2ed953c7caeecf377a8942ef78929befd5cf267b204369f7f695c6402d4818175864'
